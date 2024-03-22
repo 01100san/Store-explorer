8b7b01b8fb89fe327697d7d89f9ee9c9d5397a63
zhl <zhl3600274714@foxmail.com>
2024-03-22 20:17:33 +0800

    *

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,29 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -16,5 +35,73 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        String key = CACHE_SHOP_KEY + id;
+        // 从 Redis 中查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 存在，直接返回
+        if (StrUtil.isNotBlank(shopJson)){
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        // 不存在，查询数据库
+        Shop shop = getById(id);
+        // -- 不存在，返回错误
+        if (shop == null){
+            return Result.fail("店铺不存在");
+        }
+        // -- 存在，写入 Redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        // 返回
+        return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if (id == null){
+            return Result.fail("店铺id不能为空");
+        }
+        //1. 更新数据库
+        updateById(shop);
+        //2. 删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
+        return Result.ok();
+    }
+
+    /*
+    // 想通过 hash 的方式存储商铺信息
+    @Override
+    public Result queryById(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        // 从 Redis 中查询商铺缓存
+        Map<Object, Object> shopMap = stringRedisTemplate.opsForHash().entries(key);
+        log.info("商铺信息：{}",shopMap);
+        // 存在
+        if (!shopMap.isEmpty()){
+            Shop shop = BeanUtil.mapToBean(shopMap, Shop.class, true, CopyOptions.create());
+
+            return Result.ok(shop);
+        }
+        // 不存在，查询数据库
+        Shop shop = getById(id);
+        // -- 不存在
+        if (shop == null){
+            return Result.fail("店铺不存在");
+        }
+        // -- 存在，写入 Redis
+        Map<String, Object> map = BeanUtil.beanToMap(shop, new HashMap<>(),
+                CopyOptions.create().setIgnoreNullValue(true).setFieldValueEditor((fieldName, fieldValue) -> fieldValue +"" ));
+
+        stringRedisTemplate.opsForHash().putAll(key, map);
+        // 返回
+        return Result.ok(shop);
+    }*/
+
 
 }
--- src/main/java/com/hmdp/utils/RedisConstants.java
+++ src/main/java/com/hmdp/utils/RedisConstants.java
@@ -5,7 +5,7 @@ public class RedisConstants {
     public static final Long LOGIN_CODE_TTL = 2L;
     public static final String LOGIN_USER_KEY = "login:token:";
     public static final Long LOGIN_USER_TTL = 30L;
-
+    public static final String CACHE_SHOP_TYPE_LIST = "cache:shop-type:list";
     public static final Long CACHE_NULL_TTL = 2L;
 
     public static final Long CACHE_SHOP_TTL = 30L;
