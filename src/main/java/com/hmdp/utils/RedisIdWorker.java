c36c612c6afa2327e8d75af10ff573c8b9af19d9
zhl <zhl3600274714@foxmail.com>
2024-03-24 11:01:28 +0800

    *

--- src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -1,6 +1,12 @@
 package com.hmdp.utils;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 /**
  * ClassName: RedisIdWorker
@@ -13,5 +19,34 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class RedisIdWorker {
+    /**
+     * 开始时间戳
+     */
+    private static final long BEGIN_TIMESTAMP = 1704844800L;
+    /**
+     * 序列号的位数
+     */
+    private static final int COUNT_BITS = 32;
 
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public long nextId(String keyPrefix){
+        // 生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timeStamp = nowSecond - BEGIN_TIMESTAMP;
+
+        // 生成序列号
+        // --获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        // --自增长
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        // 拼接并返回
+        return timeStamp << COUNT_BITS | count;
+    }
 }
