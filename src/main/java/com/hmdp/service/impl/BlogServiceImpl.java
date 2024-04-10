ed1df80588fbb46fa17080205a51671ab4f1e0b4
zhl <zhl3600274714@foxmail.com>
2024-04-10 18:48:16 +0800

    *

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -5,28 +5,31 @@ import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.ScrollResult;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.Follow;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IFollowService;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.UnknownFormatConversionException;
+import javax.management.MXBean;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
+import static com.hmdp.utils.RedisConstants.FEED_KEY;
 import static java.util.Arrays.stream;
 
 /**
@@ -43,6 +46,8 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     private IUserService userService;
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private IFollowService followService;
 
     @Override
     public Result queryBlogById(Integer current) {
@@ -142,6 +147,79 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
                 .collect(Collectors.toList());
         return Result.ok(userDTOS);
     }
+
+    /**
+     * 保存探店博客到数据库，
+     * 使用 Redis 的 sorted_set 结构存储探店博客
+     */
+    @Override
+    public Result saveBlog(Blog blog) {
+        // 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        blog.setUserId(user.getId());
+        // 保存探店笔记
+        boolean isSuccess = save(blog);
+        if (!isSuccess){
+            return Result.fail("创建探店笔记失败");
+        }
+        // 找到当前用户的所有粉丝
+        // select * from tb_follow where follow_user_id = ?
+        List<Follow> followUsers = followService.query().eq("follow_user_id", user.getId()).list();
+        for (Follow follow : followUsers){
+            // 获取粉丝id
+            Long userId = follow.getUserId();
+            // 推送给粉丝
+            String key = FEED_KEY + userId;
+            stringRedisTemplate.opsForZSet().add(key, blog.getId().toString(), System.currentTimeMillis());
+        }
+        // 返回 blog id
+        return Result.ok(blog.getId());
+    }
+
+    @Override
+    public Result queryBlogOfFollow(Long max, Integer offset) {
+        // 获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        // 查询收件箱 zrevrangebyscore key max min withscores limit offset count
+        String key = FEED_KEY + userId;
+        Set<ZSetOperations.TypedTuple<String>> typedTuples = stringRedisTemplate.opsForZSet()
+                .reverseRangeByScoreWithScores(key, 0, max, offset, 2);
+        if (typedTuples == null || typedTuples.isEmpty() ) {
+            return Result.ok();
+        }
+        // 解析收件箱    blogId、offset（时间戳）
+        List<Long> ids = new ArrayList<>(typedTuples.size());
+        int newOffSet = 1;
+        long minTime = 0;
+        //------------------这里的实现有问题，计算的 offset 是上一个集合的重复个数，不是总集合的重复个数----------------
+        for (ZSetOperations.TypedTuple<String> tuple : typedTuples){
+            // 获取id
+            ids.add(Long.valueOf(tuple.getValue()));
+            // 获取分数（时间戳）
+            long time = tuple.getScore().longValue();
+            if(time == minTime){
+                newOffSet ++;
+            } else {
+                minTime = time;
+                newOffSet = 1;
+            }
+        }
+        String idStr = StrUtil.join(",", ids);
+        List<Blog> blogs = query().in("id",ids).last("ORDER BY FIELD(id," + idStr + ")").list();
+        for (Blog blog : blogs) {
+            // 查询 blog 的用户信息
+            queryBlogUser(blog);
+            // 查询 blog 是否被点赞
+            isBlogLiked(blog);
+        }
+        ScrollResult r = new ScrollResult();
+        r.setList(blogs);
+        r.setOffset(newOffSet);
+        r.setMinTime(minTime);
+        // 返回封装过的 ScrollResult
+        return Result.ok(r);
+    }
+
 
     private void queryBlogUser(Blog blog) {
         Long userId = blog.getUserId();
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,26 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.crypto.symmetric.fpe.FPE;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -16,5 +32,61 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private IUserService userService;
 
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+        if (isFollow){
+            // 关注，新增数据
+            Follow follow = new Follow();
+            follow.setFollowUserId(followUserId);
+            follow.setUserId(userId);
+            boolean isSuccess = save(follow);
+            if (isSuccess){
+                // 新增关注成功，在Redis中使用 set 集合的方式再次存储一份，得到共同关注
+                // sadd follows:userId followUserId
+                stringRedisTemplate.opsForSet().add(key, followUserId.toString());
+            }
+        }else {
+            // 取关，删除数据
+            // delete from tb_follow where user_id = ? and follow_user_id = ?
+            boolean isSuccess = remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId).eq("follow_user_id", followUserId));
+            if (isSuccess){
+                // 取消关注成功，在Redis中移除对应的关注用户id
+                stringRedisTemplate.opsForSet().remove(key, followUserId.toString());
+            }
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long followUserId) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+        return Result.ok(count > 0);
+    }
+
+    @Override
+    public Result followCommons(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        String userKey = "follows:" + userId;
+        String followKey = "follows:" + id;
+        Set<String> intersect = stringRedisTemplate.opsForSet().intersect(userKey, followKey);
+        if (intersect == null || intersect.isEmpty()){
+            return Result.ok(Collections.emptyList());
+        }
+        List<Long> ids = intersect.stream().map(Long::valueOf).collect(Collectors.toList());
+        List<UserDTO> userDTOS = userService.listByIds(ids)
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+
+        return Result.ok(userDTOS);
+    }
 }
